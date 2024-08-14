0bcdf122432bbe24948e375bc0eeabbec4702fbf
Loic Jeannin <inklo.ca@gmail.com>
2024-08-14 13:47:59 -0400

    remove sidebar when we click on the page of the website

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-// Créer un événement au click sur le hamburger menu pour sortir la sidebar, ranger la sidebar si on reclick dessus
-const sideBar = document.getElementById(`side-bar`)// important de creer la variable car JS ne va pas comprendre la class side-bar, donc on la déclare
+
+const sideBar = document.getElementById(`side-bar`);// important de creer la variable car JS ne va pas comprendre la class side-bar, donc on la déclare
+
+const content = document.querySelector(".content");
 
 btn.addEventListener("click", () =>{
+  //nous ajoutons la class "active du css" en toggle (si elle n'est pas la on l'ajoute et inversement)
   sideBar.classList.toggle("active");
+
 });
 
 // Ranger la sidebar si on click sur le contenu principal
+content.addEventListener("click", () =>{
+  sideBar.classList.remove("active");
+});
 
 // BONUS
 // Créer en css l'hamburger menu qui se transforme en croix
