d370b080be07730cc02ccbe2a6ac3cf066c2233d
Loic Jeannin <inklo.ca@gmail.com>
2024-08-14 23:06:32 -0400

    change de span when you close de sidebar

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-
-const sideBar = document.getElementById(`side-bar`);// important de creer la variable car JS ne va pas comprendre la class side-bar, donc on la déclare
+// important de creer la variable car JS ne va pas comprendre la class side-bar, donc on la déclare
+const sideBar = document.getElementById(`side-bar`);
 
 const content = document.querySelector(".content");
 
 btn.addEventListener("click", () =>{
   //nous ajoutons la class "active du css" en toggle (si elle n'est pas la on l'ajoute et inversement)
   sideBar.classList.toggle("active");
-
 });
 
 // Ranger la sidebar si on click sur le contenu principal
+//.remove permet d'annuler la classe active
 content.addEventListener("click", () =>{
   sideBar.classList.remove("active");
 });
